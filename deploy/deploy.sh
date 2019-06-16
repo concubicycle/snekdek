2a4b4e1443654eb5b178d82b520f86001de61434
concubicycle <saveliyb@gmail.com>
2019-06-16 14:01:24 -0400

    deploy stuff

---
+++ deploy/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+
+USER=saveliyb
+HOST=40.117.142.224
+
+APP_DIR=..
+APP_CSPROJ=$APP_DIR/snekdek.csproj
+
+BUILD_DIR=$APP_DIR/bin/Release/netcoreapp2.2/publish/
+PUBLISH_DIR=/home/saveliyb/app
+PUBLISH_FOLDER=/home/saveliyb/app/publish
+
+
+########################
+## APP: 
+
+# Build project
+dotnet publish -c Release $APP_DIR
+
+# Stop auth service 
+# ssh $USER@$HOST 'systemctl stop snekdek.service'
+
+# Detele currently deployed folder
+ssh $USER@$HOST "rm -rf $PUBLISH_FOLDER"
+
+# Copy publish folder over to server
+scp -rp $BUILD_DIR $USER@$HOST:$PUBLISH_DIR
+
+# Start systemd service, which will restart the auth app
+ssh root@$HOST 'systemctl start snekdek.service'
+
+
+
+echo "All done!"
